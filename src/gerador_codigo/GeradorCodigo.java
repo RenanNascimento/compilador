7a5f97e6c61edcbffccec6e09130983ced204a46
BielMagal <bielpires1995@gmail.com>
2017-12-06 23:41:30 -0200

    Atualizando gerador de códigos

--- src/gerador_codigo/GeradorCodigo.java
+++ src/gerador_codigo/GeradorCodigo.java
@@ -31,18 +31,124 @@ public class GeradorCodigo {
     }
 
 
-
-    public void declararVariaveis() {
+    public void tratarDeclList() {
         int qtd = 0;
-        do {
+        while(token.tag == Tag.INT || token.tag == Tag.STR) {
+            do {
+                token = it.next();
+                variaveis.put(token.getLexeme(), offset);
+                offset++;
+                token = it.next();
+                qtd++;
+            } while (it.hasNext() && token.tag != Tag.PV);
             token = it.next();
-            variaveis.put(token.getLexeme(),offset);
-            offset++;
-            token = it.next();
-            qtd++;
-        } while (it.hasNext() && token.tag != Tag.PV);
-        // Empilha o valor inteiro 0
+        }
+            // Empilha o valor inteiro 0
         codigo += "PUSHN " + qtd + '\n';
+        token = it.next(); // Consome PV
+    }
+
+    public void tratarSimpleExpr() {
+    }
+
+    public void tratarTerm() {
+        //term                ::= factor-a  term'
+    }
+
+    public void tratarTermPrime() {
+        //term'               ::= mulop factor-a term' | λ
+    }
+
+    public void tratarFactorA() {
+        //factor-a			::= factor  |  !  factor  |  "-"  factor
+    }
+
+    public void tratarFactor() {
+        //factor          	::= identifier  |  constant  |  "("  expression  ")"
+    }
+
+    public void tratarExpression() {
+        //expression			::= simple-expr  expression'
+    }
+
+    public void tratarExpressionPrime() {
+        //expression'			::= relop  simple-expr	|	λ
+    }
+
+    public void tratarSimpleExpression() {
+        //simple-expr			::= term  simple-expr'
+    }
+
+    public void tratarSimpleExpressionPrime() {
+        //simple-expr'		::= addop term simple-expr'  | λ
+    }
+
+    public void tratarStmtList() {
+        tratarStmt();
+        tratarStmtListPrime();
+    }
+
+    public void tratarStmtListPrime() {
+        while(token.tag != Tag.END){
+            tratarStmt();
+        }
+    }
+
+    public void tratarStmt() {
+        switch (token.tag) {
+            case Tag.ID:
+                tratarAssign();
+                token = it.next(); // Consome PV
+                break;
+            case Tag.IF:
+                tratarIf();
+                break;
+            case Tag.WHILE:
+                tratarWhile();
+                break;
+            case Tag.SCAN:
+                tratarRead();
+                token = it.next(); // Consome PV
+                break;
+            case Tag.PRINT:
+                tratarWrite();
+                token = it.next(); // Consome PV
+                break;
+        }
+    }
+
+    public void tratarIf() {
+        //if-stmt				::= if  expression  then  stmt-list  if-stmt' end
+    }
+
+    public void tratarIfPrime() {
+        //if-stmt'			::=	else stmt-list | λ
+    }
+
+    public void tratarWhile() {
+        //while-stmt			::= do stmt-list stmt-sufix
+    }
+
+    public void tratarStmtSufix() {
+        //stmt-sufix			::= while expression end
+    }
+
+    public void tratarRead() {
+        token = it.next(); //Consome Scan
+        token = it.next(); //Consome AP
+        int pos = variaveis.get(token.getLexeme());
+        token = it.next(); //Consome ID
+        token = it.next(); //Consome FP
+        codigo += "READ\n";
+        codigo += "STOREL " + pos + '\n';
+    }
+
+    public void tratarWrite() {
+        token = it.next(); //Consome PRINT
+        token = it.next(); //Consome AP
+        tratarSimpleExpr();
+        token = it.next(); //Consome FP
+        codigo += "WRITES\n";
     }
 
     public void tratarAssign() {
@@ -56,46 +162,22 @@ public class GeradorCodigo {
             neg = true;
             token = it.next();
         }
-                    /*
-                    simple-expr			::= factor  term' simple-expr'
-                    simple-expr'		::= addop term simple-expr'  | λ
-                    term'               ::= mulop factor-a term' | λ
-                    factor          	::= identifier  |  constant  |  "("  expression  ")"
-                    addop				::= "+" 	|  "-"  |  "||"
-                    mulop               ::= "*" 	|  "/"  |  "&&"
-                    constant            ::= integer_const  |  literal
-                    */
+        tratarSimpleExpr();
+        if(neg) {
+            codigo += "PUSHI -1\n";
+            codigo += "MUL\n";
+        }
+        codigo += "STOREL " + pos + '\n';
     }
 
     // Varre os tokens e gera o código referente a eles
     public void gerar() {
-        // Varre os tokens
-        while (it.hasNext()) {
-            token = it.next();
-            switch (token.tag) {
-                case Tag.PRG:
-                    codigo += "START\n";
-                    break;
-                case Tag.INT:
-                case Tag.STR:
-                    declararVariaveis();
-                    break;
-                case Tag.ID:
-                    tratarAssign();
-                    break;
-                case Tag.IF:
-                    break;
-                case Tag.WHILE:
-                    break;
-                case Tag.SCAN:
-                    break;
-                case Tag.PRINT:
-                    break;
-                case Tag.END:
-                    codigo += "STOP\n";
-                    break;
-            }
-        }
+        token = it.next();
+        codigo += "START\n";
+        token = it.next();
+        tratarDeclList();
+        tratarStmtList();
+        codigo += "STOP\n";
         gravarCodigo();
     }
 
